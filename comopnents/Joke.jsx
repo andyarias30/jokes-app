2af520d42d6469cd6a3c5f1724edf7cbc73fbd97
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-07 12:43:01 -0500

    working in app;not shuffled

---
+++ comopnents/Joke.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+import styles from "../styles"
+export default function Joke({joke}){
+return(
+    <View>
+        <Text style={styles.bigText}>{joke}</Text>
+    </View>
+)
+}
